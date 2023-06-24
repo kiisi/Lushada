dcba105ea2522251038df29b065421231a10fae4
Kiisi <kiisifelix06@gmail.com>
2023-06-24 16:41:44 +0100

    Feat - Added more static rooms data

--- src/utils/data.js
+++ src/utils/data.js
@@ -8,6 +8,15 @@ import ac_6 from '../assets/ac-6.webp'
 import ac_7 from '../assets/ac-7.webp'
 import ac_8 from '../assets/ac-8.webp'
 import ac_9 from '../assets/ac-9.webp'
+import ac_10 from '../assets/ac-10.webp'
+import ac_11 from '../assets/ac-11.webp'
+import ac_12 from '../assets/ac-12.webp'
+import ac_13 from '../assets/ac-13.webp'
+import ac_14 from '../assets/ac-14.webp'
+import ac_15 from '../assets/ac-15.webp'
+import ac_16 from '../assets/ac-16.jpg'
+import ac_17 from '../assets/ac-17.jpg'
+import ac_18 from '../assets/ac-18.jpg'
 
 
 export const rooms = [
@@ -91,5 +100,86 @@ export const rooms = [
         roomType: 'Deluxe',
         price: 14000,
         vat: 1200
+    },
+    {
+        id: nanoid(),
+        img: ac_10,
+        title: 'Akure, Ondo',
+        ratings: 5,
+        roomType: 'Standard',
+        price: 30500,
+        vat: 5200
+    },
+    {
+        id: nanoid(),
+        img: ac_11,
+        title: 'Ikeja, Lagos',
+        ratings: 4,
+        roomType: 'Deluxe',
+        price: 12000,
+        vat: 1000
+    },
+    {
+        id: nanoid(),
+        img: ac_12,
+        title: 'Badagry, Lagos',
+        ratings: 4,
+        roomType: 'Suite',
+        price: 10500,
+        vat: 1000
+    },
+    {
+        id: nanoid(),
+        img: ac_13,
+        title: 'Okrika, Rivers',
+        ratings: 4,
+        roomType: 'Suite',
+        price: 9000,
+        vat: 600
+    },
+    {
+        id: nanoid(),
+        img: ac_14,
+        title: 'Lekki, Lagos',
+        ratings: 3,
+        roomType: 'Suite',
+        price: 5000,
+        vat: 300
+    },
+    {
+        id: nanoid(),
+        img: ac_15,
+        title: 'Okrika, Rivers',
+        ratings: 5,
+        roomType: 'Standard',
+        price: 45000,
+        vat: 6000
+    },
+    {
+        id: nanoid(),
+        img: ac_16,
+        title: 'PH, Rivers',
+        ratings: 5,
+        roomType: 'Standard',
+        price: 55000,
+        vat: 8000
+    },
+    {
+        id: nanoid(),
+        img: ac_17,
+        title: 'Mainland, Lagos',
+        ratings: 5,
+        roomType: 'Deluxe',
+        price: 18000,
+        vat: 2000
+    },
+    {
+        id: nanoid(),
+        img: ac_18,
+        title: 'Mainland, Lagos',
+        ratings: 5,
+        roomType: 'Deluxe',
+        price: 25000,
+        vat: 3000
     },
 ]
